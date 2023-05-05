b18fdd777ccb7aace516318cbcce615d097e772a
danendrawm <ndaamustofa021@gmail.com>
2023-05-05 20:45:02 +0700

    first commit

---
+++ application/views/view-form-matakuliah.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Membuat CRUD dengan CodeIgniter</title>
+</head>
+
+<body>
+	<center>
+		<?= validation_errors() ?>
+		<form action="<?php echo base_url() . 'index.php/Matakuliah/cetak' ?>" method="post">
+			<table>
+				<tr>
+					<th colspan="3">
+						Form Input Data Mata Kuliah
+					</th>
+				</tr>
+				<tr>
+					<td colspan="3">
+						<hr>
+					</td>
+				</tr>
+				<tr>
+					<th>Kode</th>
+					<th>:</th>
+					<td>
+						<input type="text" name="kode" id="kode">
+					</td>
+				</tr>
+				<tr>
+					<th>Nama</th>
+					<td>:</td>
+					<td>
+						<input type="text" name="nama" id="nama">
+					</td>
+				</tr>
+				<tr>
+					<th>SKS</th>
+					<td>:</td>
+					<td>
+						<select name="sks" id="sks">
+							<option value="">Pilih SKS</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="3" align="center">
+						<input type="submit" value="Submit">
+					</td>
+				</tr>
+			</table>
+		</form>
+	</center>
+</body>
+
+</html>
